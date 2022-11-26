97918ae75de5b06b3f57b79baa66aae97a817934
Matthias Kreier <matthias@kreier.org>
2022-11-26 21:56:43 +0700

    Initial Commit

---
+++ Food PMH/Food_PMHApp.swift
@@ -0,0 +1,17 @@
+//
+//  Food_PMHApp.swift
+//  Food PMH
+//
+//  Created by Matthias Kreier on 26/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct Food_PMHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
